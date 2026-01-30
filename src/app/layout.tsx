85879a527de9e339b4055a3ad6d4168a26d37776
Tanvir Ahmed <mta.coder97@gmail.com>
2026-01-30 02:55:22 +0000

    docs: Updated the whole documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { primaryFont } from "@/constants/fonts";
 export const metadata: Metadata = {
   title: "GoBetterAuth",
   description:
-    "GoBetterAuth is an open-source authentication solution that scales with you. Embed it as a library in your Go app, or run it as a standalone auth server for any language or framework.",
+    "GoBetterAuth is an open-source authentication solution that scales with you. Embed it as a library in your Go app, or run it as a standalone auth server with any tech stack.",
 };
 
 export default function Layout({ children }: LayoutProps<"/">) {
