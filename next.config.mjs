936bbc1660220e3e3ad3a30f33e36f01230efdf7
Tanvir Ahmed <mta.coder97@gmail.com>
2026-02-01 02:32:23 +0000

    chore: Updated to fumadocs-mdx v14 and docs now support markdown copy button for LLMs

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import { createMDX } from "fumadocs-mdx/next";
+
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {
+  reactStrictMode: true,
+  output: "standalone",
+  reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/docs/:path*.mdx",
+        destination: "/llms.mdx/docs/:path*",
+      },
+    ];
+  },
+};
+
+export default withMDX(config);
